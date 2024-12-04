0bd8202a9d01c918f55e0c69406b82dda114f298
ashmi <ashmi.chaudhary@patancollege.edu.np>
2024-12-04 23:50:02 +0545

    [feature] stay logged in until logged out

--- big_bites/lib/pages/splash.dart
+++ big_bites/lib/pages/splash.dart
@@ -1,5 +1,7 @@
 import 'package:big_bites/pages/dashboard_page/dashboard_page.dart';
 import 'package:big_bites/pages/sign_in_page/sign_in.dart';
+import 'package:big_bites/services/auth_user_repo.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
@@ -19,6 +21,13 @@ class _SplashState extends State<Splash> {
 
   @override
   Widget build(BuildContext context) {
-    return SignInPage();
+    return StreamBuilder(
+        stream: AuthUserRepo.auth.authStateChanges(),
+        builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
+          if (snapshot.hasData) {
+            return DashboardPage();
+          }
+          return SignInPage();
+        });
   }
 }
