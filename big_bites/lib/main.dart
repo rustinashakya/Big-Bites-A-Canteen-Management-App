73cc9f0c2273246e1a172d641cc93634a57abefd
RuShakya <shakyarustina012@gmail.com>
2024-12-04 11:06:19 +0545

    Authentication Sign in and Sign Up in progress

--- big_bites/lib/main.dart
+++ big_bites/lib/main.dart
@@ -1,33 +1,15 @@
-import 'package:big_bites/pages/dashboard_page/dashboard_page.dart';
-import 'package:big_bites/pages/detail_page/detail_page.dart';
-import 'package:big_bites/pages/splash.dart';
+import 'package:big_bites/app.dart';
+import 'package:big_bites/simple_bloc_observer.dart';
+import 'package:bloc/bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:user_repository/user_repository.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  Bloc.observer = SimpleBlocObserver();
+  runApp(MyApp(FirebaseUserRepo()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-        title: 'Big Bites - A Canteen Management App',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: Splash(),
-        // home: const DashboardPage()
-        // home: CreateAnAccountPage(),
-        // home: WelcomePage(),
-        );
-  }
-}
