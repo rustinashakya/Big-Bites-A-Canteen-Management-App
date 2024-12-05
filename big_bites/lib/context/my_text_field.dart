d48d8bce94209ed81a69ed086cbd1c4d1a72ab54
RuShakya <shakyarustina012@gmail.com>
2024-12-05 11:36:08 +0545

    Sign Up Successfully Completed

--- big_bites/lib/context/my_text_field.dart
+++ big_bites/lib/context/my_text_field.dart
@@ -50,7 +50,7 @@ class MyTextField extends StatelessWidget {
 					borderRadius: BorderRadius.circular(20),
 					borderSide: BorderSide(color: Theme.of(context).colorScheme.secondary),
 				),
-				fillColor: Colors.grey.shade200,
+				fillColor: Colors.white,
 				filled: true,
 				hintText: hintText,
 				hintStyle: TextStyle(color: Colors.grey[500]),
